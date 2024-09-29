186aa2e9857de1e438c7bc1634a61702309462b9
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-29 21:50:10 +0200

    added MovieDetailsPage

---
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,33 @@
+import { BASE_IMG_URL } from '../../api/constants';
+
+import styles from './MovieInfo.module.css';
+
+const MovieInfo = ({
+  item: { id, poster_path, original_title, overview, genres },
+}) => {
+  return (
+    <div className={styles.section}>
+      <div className={styles.wrapper}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.img}
+            src={BASE_IMG_URL + '/w500/' + poster_path}
+            alt={original_title}
+          />
+        </div>
+        <div className={styles.infoContainer}>
+          <h4 className={styles.title}>
+            {original_title}
+            {-id}
+          </h4>
+          <p>User Score: 73%</p>
+          <h5 className={styles.subtitle}>Overview</h5>
+          <p>{overview}</p>
+          <h5 className={styles.subtitle}>Genres:</h5>
+          <p>{genres && genres.map(({ name }) => name).join(' ')}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MovieInfo;
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,58 @@
-
+import { useParams, NavLink, Outlet } from 'react-router-dom';
+import { useState, useEffect, Suspense } from 'react';
+import { useLocation } from 'react-router-dom';
+import useFetchData from '../../hooks/useFetchData';
+import ApiService from '../../api/ApiService';
 import AppContainer from '../../components/AppContainer/AppContainer';
+import AppSection from '../../components/AppSection/AppSection';
+import AppSecTitle from '../../components/AppSecTitle/AppSecTitle';
+import InfinityLoader from '../../components/Infinity/Infinity';
+import MovieInfo from '../../components/MovieInfo/MovieInfo';
+import styles from './MovieDetails.module.css';
 
 const MovieDetailsPage = () => {
+  const { id } = useParams();
+  const [item, setItem] = useState({});
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/movies';
+
+  const [itemLoading, itemError, fetchItemData] = useFetchData(async id => {
+    const responce = await ApiService.getMovieDetailsById(id);
+    setItem(responce);
+  });
+
+  useEffect(() => handleById(), []);
+
+  const handleById = () => {
+    fetchItemData(id);
+  };
 
   return (
     <AppContainer>
-    <p>MovieDetailsPage</p>
+       <AppSection>
+       <InfinityLoader isLoading={itemLoading} />
+       {itemError ? <p>{itemError}</p> : <MovieInfo item={item} />}
+    </AppSection>
+    <AppSection>
+        <AppSecTitle>Aditional information</AppSecTitle>
+        <hr></hr>
+        <nav className={styles.navLink}>
+          <NavLink className={styles.link} to={`cast`} state={location.state}>
+            Cast
+          </NavLink>
+          <NavLink
+            className={styles.link}
+            to={`reviews`}
+            state={location.state}
+          >
+            Reviews
+          </NavLink>
+        </nav>
+        <hr></hr>
+        <Suspense fallback={<div>Loading subpage...</div>}>
+          <Outlet />
+        </Suspense>
+      </AppSection>
     </AppContainer>
   );
 };
