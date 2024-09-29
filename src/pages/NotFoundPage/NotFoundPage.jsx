2361631de259583cf6ec9ba480a8f95ebb8dc30c
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-29 22:40:25 +0200

    added NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,16 @@
-
 import AppContainer from '../../components/AppContainer/AppContainer';
+import AppSection from '../../components/AppSection/AppSection';
+import AppSecTitle from '../../components/AppSecTitle/AppSecTitle';
+import styles from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-
   return (
     <AppContainer>
-      <p>NotFoundPage</p>
+      <AppSection>
+        <div className={styles.container}>
+        <AppSecTitle>Page Not Found</AppSecTitle>
+        </div>
+      </AppSection>
     </AppContainer>
   );
 };
