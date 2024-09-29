0846efb71c408834a48bd72a36fc9e24b167d33d
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-30 00:14:00 +0200

    added .env and .env.example

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,5 +1,4 @@
 export const BASE_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = '651e566d8ae118df4f6327d00acde3c1';
-
+export const API_KEY = import.meta.env.VITE_API_KEY;
 export const LOCAL_CONFIGURATION = 'tmdbConfig';
 export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/';
